2063a57aec4c16e66823ecfa07ac4143e336018b
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2026-01-07 19:16:33 +0530

    Add practice.py with variable declarations and prints

---
+++ Day1/python-basics-variables-data-types/practice.py
@@ -0,0 +1,11 @@
+city = "Mumbai"
+year = 2026
+temperature = 32.5
+is_raining = False
+
+print("City:", city)
+print("Year:", year)
+print("Temperature:", temperature)
+print("Is Raining:", is_raining)
+
+print(type(city), type(year), type(temperature), type(is_raining))
