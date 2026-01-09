7cb3120b7ad495d4eaa154ba30ab5c911538e8ed
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2026-01-09 11:37:51 +0530

    Create 4.py

---
+++ Day3/Strings and Conditional Statements (if, elif, else)/Strings in Python/String Methods/4.py
@@ -0,0 +1,10 @@
+msg = "python programming"
+
+
+print(msg.upper())
+print(msg.lower())
+print(msg.title())
+print(msg.capitalize())
+print(msg.replace("python", "java"))
+print(msg.find("program"))
+print(len(msg))
